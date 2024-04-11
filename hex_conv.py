42643329802372d5d459e6f38088e528e7fce946
pol <140607340+polmt@users.noreply.github.com>
2024-04-11 22:07:57 +0300

    Add files via upload

---
+++ hex_conv.py
@@ -0,0 +1,28 @@
+import struct
+
+# Use 'rb' mode for reading binary files
+with open(r'C:\Users\metal\TEST.BIN', 'rb') as read_file:
+    with open(r'C:\Users\metal\TEST.txt', 'w') as write_file:
+        # Read 4 bytes at a time from the binary file
+        chunk = read_file.read(4)
+        count = 0
+
+        while chunk:
+            # Convert from Big Endian to Little Endian (4 bytes = 'I' format)
+            little_endian_value = struct.unpack('<I', chunk)[0]
+
+            # Formatting for new lines and tabs
+            if count % 4 == 0:
+                if count > 0:
+                    write_file.write('\n')
+                write_file.write('\t')
+
+            # Write the converted value in hexadecimal format
+            write_file.write(f'X"{little_endian_value:08X}", ')
+
+            # Read the next 4 bytes
+            chunk = read_file.read(4)
+            count += 1
+
+        # Ensure the last comma is not a problem for whatever will use this file
+        # This step might depend on how the output file is intended to be used
